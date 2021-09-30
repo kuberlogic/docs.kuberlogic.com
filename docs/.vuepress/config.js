9d17c040d9097ce14b4fbfe2cdcc8f1246e3b57c
jdaveyancloudlinux <86103906+jdaveyancloudlinux@users.noreply.github.com>
2021-09-30 14:26:15 +0400

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -39,7 +39,7 @@ module.exports = {
       }
     }
   },
-  base: "/",
+  base: "/kuberlogic-doc/",
   head: [
     ["link", { rel: "icon", href: "/favicon.png" }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
